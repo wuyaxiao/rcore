f71d457d7e8b827c11d869bf4ee5de469ab4f023
oslab <2270054562@qq.com>
2024-04-25 23:48:16 +0800

    Committing changes before switching branches

--- os/src/batch.rs
+++ os/src/batch.rs
@@ -15,15 +15,15 @@ struct KernelStack {
 }
 
 #[repr(align(4096))]
-struct UserStack {
+pub struct UserStack {
     data: [u8; USER_STACK_SIZE],
 }
 
 static KERNEL_STACK: KernelStack = KernelStack { data: [0; KERNEL_STACK_SIZE] };
-static USER_STACK: UserStack = UserStack { data: [0; USER_STACK_SIZE] };
+pub static USER_STACK: UserStack = UserStack { data: [0; USER_STACK_SIZE] };
 
 impl KernelStack {
-    fn get_sp(&self) -> usize {
+    pub fn get_sp(&self) -> usize {
         self.data.as_ptr() as usize + KERNEL_STACK_SIZE
     }
     pub fn push_context(&self, cx: TrapContext) -> &'static mut TrapContext {
@@ -34,7 +34,7 @@ impl KernelStack {
 }
 
 impl UserStack {
-    fn get_sp(&self) -> usize {
+    pub fn get_sp(&self) -> usize {
         self.data.as_ptr() as usize + USER_STACK_SIZE
     }
 }
